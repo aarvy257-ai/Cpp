b9144e2cbd9a84de4af203ae5d97ae7c36a03fc4
Aarvy Agrawal <aarvy257@gmail.com>
2026-08-13 22:43:51 +0530

    add

---
+++ 01_Basics/009_size_data_types.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int inttype;
+    float floattype;
+    double doubletype;
+    cout << " Size of int: " << sizeof(inttype)<<"Bytes"<<endl;
+    cout << " Size of float: " << sizeof(floattype)<<"Bytes"<<endl;
+    cout << " Size of double: " << sizeof(doubletype)<<"Bytes"<<endl;
+    return 0;
+}
