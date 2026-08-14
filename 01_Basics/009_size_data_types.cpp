134fb2032e76df53269c869a8740041cbc61d0b3
Aarvy Agrawal <aarvy257@gmail.com>
2026-08-15 00:40:08 +0530

    add

--- 01_Basics/009_size_data_types.cpp
+++ 01_Basics/009_size_data_types.cpp
@@ -7,5 +7,6 @@ int main(){
     cout << " Size of int: " << sizeof(inttype)<<"Bytes"<<endl;
     cout << " Size of float: " << sizeof(floattype)<<"Bytes"<<endl;
     cout << " Size of double: " << sizeof(doubletype)<<"Bytes"<<endl;
+    cout<< " Size of char "
     return 0;
 }
---
+++ 02_If_Else/003_vowel_consonant.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char a;
+    cout<<"Enter a character : ";
+    cin>>a;
+    a=
+
+}
