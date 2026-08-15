d8264a0c506ae2b7fd212c387a4b3e0450fcdd0d
Aarvy Agrawal <aarvy257@gmail.com>
2026-08-15 23:56:15 +0530

    add

--- 02_If_Else/003_vowel_consonant.cpp
+++ 02_If_Else/003_vowel_consonant.cpp
@@ -6,6 +6,13 @@ int main(){
     char a;
     cout<<"Enter a character : ";
     cin>>a;
-    a=
-
+    if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U')
+    {
+        cout<<a<<" is a VOWEL";
+    }
+    else
+    {
+        cout<<a<<" is a CONSONANT";
+    }
+return 0;
 }
